8670ede5105a370ad9664fbe5ae61bacb3e3ccc1
ruoshui <ruoshuim@gmail.com>
2024-02-07 05:57:57 -0400

    fix: reduce page reload frequency when insufficient privilege

--- src/components/meeting/InsufficientPrivilege.svelte
+++ src/components/meeting/InsufficientPrivilege.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-    setTimeout(() => {
-        window.location.reload();
-    }, 1000);
+  import zoomSdk from '@zoom/appssdk';
+
+  zoomSdk.onMyUserContextChange((e) => {
+    if (e.role === 'host' || e.role === 'coHost') {
+      window.location.reload();
+    }
+  });
+  // setTimeout(() => {
+  //     window.location.reload();
+  // }, 1000);
 </script>
 
-<h2 class="error">需要 Host 或 Co-Host 权限才能使用此功能。获得权限后请刷新重试。（此页面会自动刷新）</h2>
+<h2 class="error">
+  需要 Host 或 Co-Host
+  权限才能使用此功能。获得权限后请刷新重试。（权限更改时，此页面会自动刷新）
+</h2>
 
 <style>
-    .error {
-        color: red;
-    }
+  .error {
+    color: red;
+  }
 </style>
